b979edc12351370dd887ac04c11268d5662bee50
Miloni Atal <miloniatal@gmail.com>
2023-05-23 10:50:37 -0400

    works for example

--- include/constants.cuh
+++ include/constants.cuh
@@ -1,11 +1,10 @@
 #pragma once
-
 #include <cstdint>
 #include <cuda_runtime.h>
 
 namespace pcg_constants{
-    extern uint32_t DEFAULT_MAX_PCG_ITER;
-    extern float DEFAULT_EPSILON;
-    extern dim3 DEFAULT_GRID;
-    extern dim3 DEFAULT_BLOCK;
+    uint32_t DEFAULT_MAX_PCG_ITER = 100;
+    float DEFAULT_EPSILON = 1e-6;
+    dim3 DEFAULT_GRID(128);
+    dim3 DEFAULT_BLOCK(64);
 }
--- src/constants.cu
+++
@@ -1,8 +0,0 @@
-#include "constants.cuh"
-
-namespace pcg_constants{
-    uint32_t DEFAULT_MAX_PCG_ITER = 500;
-    float DEFAULT_EPSILON = 1e-6;
-    dim3 DEFAULT_GRID(128);
-    dim3 DEFAULT_BLOCK(64);
-}
